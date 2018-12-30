2789a43cfa5961b04676f94c591a10a7a1fc5d7e
Sean Coyne <scoyne2@kent.edu>
2018-12-30 06:00:01 -0800

    better use of templates?

--- DataDirectorApp/settings.py
+++ DataDirectorApp/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'DataDirectorApp',
-    'fileupload'
+    'fileupload',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- fileupload/views.py
+++ fileupload/views.py
@@ -1,10 +1,6 @@
 from .forms import FileUploadForm
 from django.shortcuts import render
 
-def fileupload2(request):
-    form = FileUploadForm()
-    return render(request, 'fileupload.html', {'form': form})
-
 def fileupload(request):
     if request.method == 'POST':
         form = FileUploadForm(request.POST, request.FILES)
