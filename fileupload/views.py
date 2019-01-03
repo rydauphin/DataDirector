612eab26f8f205e76e4086ab55ad5d7c89053ee0
Sean Coyne <scoyne2@kent.edu>
2019-01-03 13:46:05 -0800

    Correct the form upload

--- fileupload/views.py
+++ fileupload/views.py
@@ -1,12 +1,18 @@
 from .forms import FileUploadForm
 from django.shortcuts import render
+from django.contrib import messages
+
 
 def fileupload(request):
     if request.method == 'POST':
         form = FileUploadForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            messages.success(request, 'Form submission successful')
+        else:
+            messages.error(request, 'Yikes! Something went wrong!')
+
+
     else:
         form = FileUploadForm()
     return render(request, 'fileupload.html', {
